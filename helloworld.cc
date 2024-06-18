9248df46472d2817ace849c0f59ee1ccf26d367e
1203318 <1203318@learn.vsb.bc.ca>
2024-06-18 10:28:54 -0700

    Add files via upload

---
+++ helloworld.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//comment
+/*
+multi
+* 
+line
+comment
+*/
+
+int main(){
+    cout<<"hello world"<<endl;
+    //cout<<"hello world\n"; //same as above
+    //std::cout<<"hi"<<std::endl; //if not using namespace std
+    return 0;
+}
