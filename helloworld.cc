f68916addd27b1063f99e54f1d3511c7edc3901a
Cohwyn Cheng <1203318@learn.vsb.bc.ca>
2024-06-18 11:11:09 -0700

    made it so theres TWO hello worls

--- helloworld.cc
+++ helloworld.cc
@@ -1,17 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//comment
-/*
-multi
-* 
-line
-comment
-*/
-
 int main(){
-    cout<<"hello world"<<endl;
-    //cout<<"hello world\n"; //same as above
-    //std::cout<<"hi"<<std::endl; //if not using namespace std
+    cout<<"hello world hello WORLD"<<endl;
     return 0;
 }
